2950a57834dd691bed811042a78f2f7c4986ca51
Rebble69 <maximilian290807@gmail.com>
2022-06-11 03:15:46 +0700

    added ui to create
    its shit lmao

---
+++ src/pages/api/createshort.ts
@@ -0,0 +1,23 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "../../db/client";
+
+interface reqBody {
+  url: string;
+  slug: string;
+}
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { url, slug } = req.body as reqBody;
+
+  if (!url || !slug) {
+    return res.status(400).json({ message: "enter all fields" });
+  }
+
+  await prisma.shortLinks.create({ data: { url, slug } }).catch((reason) => {
+    return res.status(502).json({ message: "this slug already exists, noob" });
+  });
+
+  return res.redirect("/createslug");
+};
+
+export default handler;
