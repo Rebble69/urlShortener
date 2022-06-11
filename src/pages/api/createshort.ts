f542b1919bae46a8d2cc0d79567371a795bba462
Rebble69 <maximilian290807@gmail.com>
2022-06-11 15:29:08 +0700

    fixxy

--- src/pages/api/createshort.ts
+++ src/pages/api/createshort.ts
@@ -17,7 +17,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     return res.status(502).json({ message: "this slug already exists, noob" });
   });
 
-  return res.status(200).redirect("/createshort");
+  return res.redirect("/createshort");
 };
 
 export default handler;
